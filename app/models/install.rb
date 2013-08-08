f55ef64257832223b41e2bc090ea0685e656fbe9
LukeWChang <wooyoung2468@gmail.com>
2013-08-08 00:07:12 -0700

    Making minor changes

--- app/models/install.rb
+++ app/models/install.rb
@@ -2,6 +2,6 @@ class Install < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,#:recoverable
+          :rememberable, :trackable, :validatable
 end

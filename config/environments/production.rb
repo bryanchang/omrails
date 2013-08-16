27263ee1a97910e383bf83bc3f4438a77bfb47e9
LukeWChang <wooyoung2468@gmail.com>
2013-08-15 23:21:25 -0700

    WORK

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
    config.paperclip_defaults = {
     :storage => :s3,
-    :s3_protocol => 'http',
     :bucket => ENV['AWS_BUCKET'],
     :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
